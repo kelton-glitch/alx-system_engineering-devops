ece0e2cdf54edd28dc6fe115b90d20988ab081f1
Kamgo N Elton <kamgoelton89@gmail.com>
2023-04-25 23:46:39 +0100

    fix

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,7 +4,7 @@ import json
 import requests
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/users/"
+    url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
 
     with open("todo_all_employees.json", "w") as jsonfile:
