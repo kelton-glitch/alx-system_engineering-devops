ca60304a9a301d4278681da0fd65e74a2da5742c
Kamgo N Elton <kamgoelton89@gmail.com>
2023-04-25 23:59:52 +0100

    fix

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -15,5 +15,5 @@ with open("todo_all_employees.json", "w") as jsonfile:
             "username": user.get("username")
             }
             for task in requests.get(url + "todos",
-                                    params={"userId": user.get("id")}).json()]
-            for user in users}, jsonfile)
+                                     params={"userId": user.get("id")}).json()]
+        for user in users}, jsonfile)
