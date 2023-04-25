62487d8f5e2dadb9b4f6a2ceda5c2c490634ba36
Kamgo N Elton <kamgoelton89@gmail.com>
2023-04-25 23:48:26 +0100

    fix

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
             u.get("id"): [{
+                "username": u.get("username"),
                 "task": t.get("title"),
                 "completed": t.get("completed"),
-                "username": u.get("username")
             } for t in requests.get(url + "todos",
                                     params={"userId": u.get("id")}).json()]
             for u in users}, jsonfile)
